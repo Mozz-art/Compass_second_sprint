f50e87efc5eb10185b31950d384fa50c78a7488d
mozzart <Brunocorona@live.com>
2022-02-09 22:14:30 -0300

    Feat: Adicionado javascript da tela de login

---
+++ index.js
@@ -0,0 +1,7 @@
+import Clima from "./scripts/Clima.js"
+import Login from "./scripts/Login.js"
+import Relogio from "./scripts/Relogio.js"
+import Logout from "./scripts/Logout.js"
+Clima()
+Login()
+Logout()
---
+++ scripts/Login.js
@@ -0,0 +1,33 @@
+import Timeout from "./Timeout.js"
+
+export default function Login() {
+
+  const inputUser = document.querySelector("#User")
+
+  const inputSenha = document.querySelector("#Senha")
+
+  const BtnContinuar = document.querySelector("#btnContinuar")
+
+  const Erro = document.querySelector("#Erro")
+
+  const Login = document.querySelector("#LOGIN")
+
+  const home = document.querySelector("#HOME")
+
+  BtnContinuar.addEventListener("click", () => {
+      
+    if (inputUser.value === "admin" && inputSenha.value === "admin") {
+        Login.style.display = "none"
+        home.style.display = "block"
+        Timeout()
+
+    } else {
+        Erro.style.display="block"
+        Login.style.display = "block"
+        home.style.display = "none"
+        inputUser.style.border = "1px solid #E9B425"
+        inputSenha.style.border = "1px solid #E9B425"
+    }
+  })
+}
+
