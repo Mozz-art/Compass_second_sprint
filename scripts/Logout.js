9d5b9373a4831e448e91b2577384fbb9554b2d60
mozzart <Brunocorona@live.com>
2022-02-09 22:35:22 -0300

    Feat: adicionado função ao botão logout com js

---
+++ scripts/Logout.js
@@ -0,0 +1,14 @@
+export default function Logout(){
+
+    const btnLogout = document.querySelector("#btnLogout")
+    const Login = document.querySelector("#LOGIN")
+    const home = document.querySelector("#HOME")
+
+    btnLogout.addEventListener("click", () => {
+        Login.style.display = "block"
+        home.style.display = "none"
+        
+    })
+
+
+}
