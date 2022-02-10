6f00b754362b3db66b46cc91ca036b6f568aacb1
mozzart <Brunocorona@live.com>
2022-02-10 12:34:09 -0300

    Fix: Weather arrumado junto ao icone

--- scripts/Clima.js
+++ scripts/Clima.js
@@ -1,32 +1,71 @@
 export default function Clima(){
 
-    const CITY = document.querySelector("#city");
-    const TEMPERATURE = document.querySelector("#temperature");
-    const URL_MAIN = 'https://api.openweathermap.org/data/2.5/weather';
-    const API_KEY = 'cadc82de74495c75da1dc074e04ca989';
-    const UNITS = 'metric';
-    
-    navigator.geolocation.getCurrentPosition(loadUrl);
-    
-    function loadUrl(pos) {
-      let lat = pos.coords.latitude;
-      let long = pos.coords.longitude;
-      console.log(lat)
-      console.log(long)
-      console.log(pos)
-      let url = `${URL_MAIN}?lat=${lat}&lon=${long}&units=${UNITS}&APPID=${API_KEY}`;
-      console.log(url)
-      fetchApi(url);
-      
-    };
-    
-    async function fetchApi(url) {
-      let response = await fetch(url);
-      let { main, name } = await response.json();
-      let temperature = (main.temp).toFixed(0);
-      CITY.innerText = `${name}:`;
-      console.log(name)
-      TEMPERATURE.innerText = `${temperature} ºC`;
-    }
-    }
-    
+  fetch("https://api.weatherapi.com/v1/current.json?key=3a8e38945c8048d4a22134953221002&q=Rio_de_Janeiro&aqi=no")
+  .then((respondes)=> respondes.json()).then(resultado =>{
+    const city = resultado.location.name
+    console.log(resultado)
+    const temp = resultado.current.temp_c
+    let icons = resultado.current.condition.icon
+    console.log(temp)
+    document.querySelector("#city").innerHTML = city + "- RJ";
+    document.querySelector("#temperature").innerHTML = temp + "°";
+    let icones =  document.querySelector("#icon")
+      icones.src = icons
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
